0a55523123b52aef0aa2ba6236ef1d7938e69f02
Adwaith Raj <adwaithraj45654@gmail.com>
2023-09-16 22:28:58 +0530

    second

--- Items/models.py
+++ Items/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=200)
+
+class Items(models.Model):
+    Id  = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places= True)
+    image = models.ImageField(blank=True ) 
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tags, blank = True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
