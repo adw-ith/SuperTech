7cd6fbbbaf6f3d39f9e442318c7a33282e2b386b
Adwaith Raj <adwaithraj45654@gmail.com>
2023-09-16 21:42:08 +0530

    First Commit

---
+++ St/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for St project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'St.settings')
+
+application = get_asgi_application()
