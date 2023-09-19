bf347bb5b9d6a90c01f4e58320a993e88f2c1694
Adwaith Raj <adwaithraj45654@gmail.com>
2023-09-19 17:38:26 +0530

    commit today

--- Items/admin.py
+++ Items/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category,Items,Tags
+from .models import Category,Items,Tags,DisplayImages
 
 
 admin.site.register(Category)
 admin.site.register(Items)
 admin.site.register(Tags)
+admin.site.register(DisplayImages)
--- Items/models.py
+++ Items/models.py
@@ -14,6 +14,10 @@ class Tags(models.Model):
     def __str__(self):
         return self.name
 
+class DisplayImages(models.Model):
+    dimg = models.ImageField(upload_to= 'static/itemimages')
+
+
 class Items(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places= True)
@@ -21,6 +25,7 @@ class Items(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tags, blank = True)
     created = models.DateTimeField(auto_now_add=True)
+    disp_imgs = models.ManyToManyField(DisplayImages)
 
     class Meta:
         ordering = ['-created']
